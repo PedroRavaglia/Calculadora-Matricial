d3b08ef29459866ef1f1c06c56f518091d50313b
RNReis <87503694+RNReis@users.noreply.github.com>
2021-09-12 17:43:26 -0300

    Update Interface Grafica.py

--- Interface Grafica.py
+++ Interface Grafica.py
@@ -33,32 +33,75 @@ vescalar.place(x=10,y=150,width=200,height=20)
 
 #Botão com cada um dos possiveis calculo de matrizes diponivel para o usuario
 
-#cada funcao1 trocar por uma fc da calculadora
-btn=Button(app,text="adição",command=funcao1)
+#cada funcao fazendo referencia aos calculos possiveis e salvando o resultado no final do arquivo
+def funcaoadd(vorigem,vprimeira,vsegunda):
+     a=index2matrix(vprimeira,vorigem)
+     b=index2matrix(vsegunda,vorigem)
+     c=__add__(l,c,a,b) #linha,coluna,matriz
+     return add_row(c,vorigem)
+btn=Button(app,text="adição",command=funcaoadd)
 btn.place(x=0,y=200,width=100,height=20)
 
-btn=Button(app,text="Inversa",command=funcao1)
+def funcaoinv(vorigem,vprimeira):
+     a=index2matrix(vprimeira,vorigem)
+     b=inv(l,c,a) #linha,coluna,matriz
+     return add_row(b,vorigem)
+btn=Button(app,text="Inversa",command=funcaoinv)
 btn.place(x=100,y=200,width=100,height=20)
 
-btn=Button(app,text="multiplicacao",command=funcao1)
+def funcaomu(vorigem,vprimeira,vsegunda):
+     a=index2matrix(vprimeira,vorigem)
+     b=index2matrix(vsegunda,vorigem)
+     c=__mul__(l,c,a,b) #linha,coluna,matriz
+     return add_row(c,vorigem)
+btn=Button(app,text="multiplicacao",command=funcaomu)
 btn.place(x=200,y=200,width=100,height=20)
 
-btn=Button(app,text="multiplicacao por escalar",command=funcao1)
+def funcaomu1(vorigem,vprimeira,vescalar):
+     a=index2matrix(vprimeira,vorigem)
+     b=index2matrix(vescalar,vorigem)
+     c=__mul__(l,c,a,b) #linha,coluna,matriz
+     return add_row(c,vorigem)
+btn=Button(app,text="multiplicacao por escalar",command=funcaomu1)
 btn.place(x=300,y=200,width=150,height=20)
 
+def funcaomu1(vorigem,vprimeira,vsegunda,vescalar):
+     a=index2matrix(vprimeira,vorigem)
+     b=index2matrix(vsegunda,vorigem)
+     c=index2matrix(vescala,vorigem)
+     d=__mul__(l,c,a,c[0])
+     e=__mul__(l,c,b,c[1])
+     f=__mul__(l,c,d,e) #linha coluna,matriz
+     return add_row(f,vorigem)
 btn=Button(app,text="Combinação linear",command=funcao1)
 btn.place(x=0,y=230,width=120,height=20)
 
-btn=Button(app,text="Transposta",command=funcao1)
+def funcaot(vorigem,vprimeira):
+     a=index2matrix(vprimeira,vorigem)
+     b=t(l,c,a) #linha,coluna,matriz
+     return add_row(b,vorigem)
+btn=Button(app,text="Transposta",command=funcaot)
 btn.place(x=120,y=230,width=100,height=20)
 
-btn=Button(app,text="Traço",command=funcao1)
+def funcaof(vorigem,vprimeira):
+     a=index2matrix(vprimeira,vorigem)
+     b=trace(l,c,a) #linha,coluna,matriz
+     return add_row(b,vorigem)
+btn=Button(app,text="Traço",command=funcaof)
 btn.place(x=200,y=230,width=100,height=20)
 
-btn=Button(app,text="Determinante",command=funcao1)
+def funcaodet(vorigem,vprimeira):
+     a=index2matrix(vprimeira,vorigem)
+     b=det(l,c,a) #linha,coluna,matriz
+     return add_row(b,vorigem)
+btn=Button(app,text="Determinante",command=funcaodet)
 btn.place(x=300,y=230,width=100,height=20)
 
-btn=Button(app,text="Diagonalização",command=funcao1)
+def funcaodiag(vorigem,vprimeira):
+     a=index2matrix(vprimeira,vorigem)
+     b=diag(l,c,a) #linha,coluna,matriz
+     return add_row(b,vorigem)
+btn=Button(app,text="Diagonalização",command=funcaodiag)
 btn.place(x=0,y=260,width=100,height=20)
 
 
